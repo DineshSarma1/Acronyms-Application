1d83838e48465d539acb728fdcfd34636ff5fa94
DineshSarma1 <dineshsarma202@gmail.com>
2022-12-02 21:09:16 -0600

    HomeViewModelTest implemented

--- app/src/test/java/com/example/acronymsapp/ui/HomeViewModelTest.kt
+++ app/src/test/java/com/example/acronymsapp/ui/HomeViewModelTest.kt
@@ -1,7 +1,37 @@
 package com.example.acronymsapp.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.acronymsapp.data.AcronymsRepository
+import com.example.acronymsapp.data.AcronymsRepositoryImpl
+import io.mockk.mockk
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 class HomeViewModelTest {
+
+    lateinit var repository: AcronymsRepository
+    lateinit var viewModel: HomeViewModel
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        repository = mockk<AcronymsRepositoryImpl>()
+        viewModel = HomeViewModel(repository)
+    }
+
+    @Test
+    fun `test Loading Status After Search Button Click - LiveDataChanged` () {
+
+        //initially loading value is false
+
+        //loading value should be true after button click
+        viewModel.onSearchButtonClicked()
+
+        assertEquals(true, viewModel.loading.value)
+    }
 
 }
